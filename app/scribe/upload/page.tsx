6171bf28d951a06d3e4d5089f7f86b5184436f2c
Nwabuksii <chibuikemgoodluck1@gmail.com>
2026-09-14 01:36:04 +0100

    Update page.tsx

--- app/scribe/upload/page.tsx
+++ app/scribe/upload/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 import { useEffect, useState, FormEvent } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { getStoredUser } from "@/lib/client-session";
